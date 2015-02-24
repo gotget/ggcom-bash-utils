1e5dcc794bf0451d25d3998572183ec753d4f46e
Louis T. Getterman IV <thad.getterman@gmail.com>
2015-02-23 19:57:10 -0600

    DNS Alias problem note

--- nukeHosts.bash
+++ nukeHosts.bash
@@ -36,8 +36,9 @@ echo "Known host file  : ${TARGET}"
 
 HOST=${1}
 HOSTIP=`host ${1} | awk '{print $4}'`
+# TO FIX: HOSTIP doesn't follow-through on aliases, and thus provides an invalid IP return
 
-echo "Nuking Host Name : ${HOST} (${HOSTIP})"
+echo "Nuking Host Name : ${HOST}"
 ssh-keygen -f ${TARGET} -R ${HOST} > /dev/null 2>&1
 
 if valid_ipv4 $HOSTIP == true; then
