853e7cf438c0f9cc954681b175856e63284b3f14
Louis T. Getterman IV <thad.getterman@gmail.com>
2015-03-16 19:36:08 -0500

    Bug fix and additional methods supported

--- sshcon.bash
+++ sshcon.bash
@@ -22,7 +22,7 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 ################################################################################
 SCRIPTPATH="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-SCRIPTNAME=`basename $SOURCE`
+SCRIPTNAME=`basename "$SOURCE"`
 LIBPATH="$( cd "$(dirname "${SCRIPTPATH}/../../")" ; pwd -P )/ggcom-bash-library"
 ################################################################################
 source "${LIBPATH}/varsBash.bash"
@@ -31,11 +31,56 @@ source "${LIBPATH}/version.bash"
 ################################################################################
 source "${LIBPATH}/colors.bash"
 ################################################################################
+ALIASNAME=`basename "$0"`
+ALIASPATH=$( cd "$(dirname "$0")" ; pwd -P )
 
 #----- VARIABLES
-USER=$SCRIPTNAME
-HOST=$(basename $SCRIPTPATH)
+SSHHOST=$(basename "$ALIASPATH")
+if [ -f "$ALIASPATH/.commonname" ]; then COMMONNAME=`cat "$ALIASPATH/.commonname"`; else COMMONNAME=`echo $SSHHOST | cut -d. -f1`; fi
+COMMONNAMELENGTH=${#COMMONNAME}
 #-----/VARIABLES
+
+# MATCH 1
+MATCH1=`echo "$ALIASNAME" | cut -c "$(( COMMONNAMELENGTH + 1 ))-"`
+
+# MATCH 2
+if [ ${#ALIASNAME} -gt $COMMONNAMELENGTH ]; then MATCH2=`echo "${ALIASNAME:0:${#ALIASNAME}-$(( COMMONNAMELENGTH + 1 ))}"`; else MATCH2=''; fi
+
+# MATCH 3
+MATCH3=`echo "${ALIASNAME: -$COMMONNAMELENGTH}"`
+
+# e.g. ./userName
+USERNAMEMODE='normal'
+# Match conditions
+if [ "$COMMONNAME" == "$MATCH3" ] && [ -z "$MATCH2" ]; then
+	# e.g. ./host
+	USERNAMEMODE='short'
+elif [ "$COMMONNAME" == "$MATCH2" ]; then
+	# e.g. ./hostRoot
+	USERNAMEMODE='medium'
+elif [ "$MATCH1" == "$MATCH3" ]; then
+	# e.g. ./hostBob
+	USERNAMEMODE='medium'
+elif [ "$COMMONNAME" == "$MATCH3" ]; then
+	# e.g. ./ user-host
+	USERNAMEMODE='long'
+fi
+
+# Connection method based upon condition
+case "$USERNAMEMODE" in
+	normal)
+		SSHUSER=$ALIASNAME
+		;;
+	short)
+		SSHUSER=${1-`whoami`}
+		;;
+	medium)
+		SSHUSER="$MATCH1"
+		;;
+	long)
+		SSHUSER="$MATCH2"
+		;;
+esac
 
 #----- NOTICE: INFO
 echo `str_repeat - 80`
@@ -46,8 +91,8 @@ echo ''
 
 #----- MAIN
 echo `str_repeat - 80`
-echo -e "Connecting: ${ggcPurple}${USER}${ggcNC}@${ggcCyan}${HOST}${ggcNC}"
+echo -e "Connecting: ${ggcPurple}${SSHUSER}${ggcNC}@${ggcCyan}${SSHHOST}${ggcNC}"
 echo `str_repeat - 80`
 
-ssh "${USER}@${HOST}"
+ssh "${SSHUSER}@${SSHHOST}"
 #-----/MAIN
